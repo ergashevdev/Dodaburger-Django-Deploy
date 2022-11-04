e3371335b4feea9d2daae2e0fa313e5fcb7b6bb5
Islom <you@example.com>
2022-11-05 04:00:09 +0500

    islom

--- tbot/settings.py
+++ tbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2l+=ec56u$u=vt07svay)s#gy95)lf58mt4(lb6%0*onnw^zv9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
