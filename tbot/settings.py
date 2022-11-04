bad4ac19d9c94b99242f827fb09ecde455c9edf5
Islom <you@example.com>
2022-11-05 03:54:22 +0500

    islom

--- tbot/settings.py
+++ tbot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2l+=ec56u$u=vt07svay)s#gy95)lf58mt4(lb6%0*onnw^zv9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
